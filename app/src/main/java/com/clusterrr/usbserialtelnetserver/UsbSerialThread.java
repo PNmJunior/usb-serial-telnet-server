a36ad06ab9a753963e5083e5c86540f16cacc2e0
PNmJunior <nachajpavel@centrum.cz>
2023-03-10 23:09:07 +0100

    improved (faster) reception of data from usb
    Modifications taken from:
    https://github.com/mik3y/usb-serial-for-android.git

--- app/src/main/java/com/clusterrr/usbserialtelnetserver/UsbSerialThread.java
+++ app/src/main/java/com/clusterrr/usbserialtelnetserver/UsbSerialThread.java
@@ -1,34 +1,61 @@
 package com.clusterrr.usbserialtelnetserver;
 
 import android.util.Log;
+import android.view.ViewDebug;
 
 import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
 
 public class UsbSerialThread extends Thread {
     final static int WRITE_TIMEOUT = 1000;
 
+    private final Object mReadBufferLock = new Object();
+    private ByteBuffer mReadBuffer;
+    private SerialInputOutputManager.Listener mListener; // Synchronized by 'this'
+    public synchronized SerialInputOutputManager.Listener getListener() {return mListener;}
     private UsbSerialTelnetService mUsbSerialTelnetService;
     private UsbSerialPort mSerialPort;
 
     public UsbSerialThread(UsbSerialTelnetService usbSerialTelnetService, UsbSerialPort serialPort) {
         mUsbSerialTelnetService = usbSerialTelnetService;
         mSerialPort = serialPort;
+        //mListener;
+        int llll = serialPort.getReadEndpoint().getMaxPacketSize();
+        Log.d("Max velikost", Integer.toString(llll));
+        mReadBuffer = ByteBuffer.allocate(serialPort.getReadEndpoint().getMaxPacketSize());
     }
 
     @Override
     public void run() {
-        byte buffer[] = new byte[1024];
+        //byte buffer[] = new byte[1024];
 
         try {
             while (true) {
+                byte[] buffer;
+                synchronized (mReadBufferLock) {
+                    buffer = mReadBuffer.array();
+                }
                 if (mSerialPort == null) break;
                 // Read data
                 int l = mSerialPort.read(buffer, 0);
                 if (l <= 0) break; // disconnect
+                int len = l;
+                if (len > 0) {
+                    if (true) {
+                        Log.d("TAG", "Read data len=" + len);
+                    }
+                    final SerialInputOutputManager.Listener listener = getListener();
+                    if (listener != null) {
+                        final byte[] data = new byte[len];
+                        System.arraycopy(buffer, 0, data, 0, len);
+                        listener.onNewData(data);
+                    }
+                }
                 // Write data
                 mUsbSerialTelnetService.writeClients(buffer, 0, l);
             }
